73f6a0a88c2346de6dbba57d6202d50b31b546d7
Jason P'ng <jsjpng@gmail.com>
2017-09-16 05:31:33 -0400

    Added imageView to results page

--- app/src/main/java/com/example/jason/shadowgame/PuzzleResult.java
+++ app/src/main/java/com/example/jason/shadowgame/PuzzleResult.java
@@ -2,12 +2,19 @@ package com.example.jason.shadowgame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class PuzzleResult extends AppCompatActivity {
 
+    TextView resultText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_puzzle_result);
+        resultText = (TextView) findViewById(R.id.result_text);
+
+        Bundle extras = getIntent().getExtras();
+        String pic = extras.getString("pic");
+        resultText.setText(pic);
     }
 }

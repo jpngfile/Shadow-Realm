c3844665657fe63f9232081ce9064d19ce229f06
Jason P'ng <jsjpng@gmail.com>
2017-09-16 03:59:54 -0400

    Added basic results page

--- app/src/main/java/com/example/jason/shadowgame/MainActivity.java
+++ app/src/main/java/com/example/jason/shadowgame/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.jason.shadowgame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button resultButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        resultButton = (Button) findViewById(R.id.resultButton);
+        resultButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               viewResults(v);
+            }
+        });
+
+    }
+
+    public void viewResults(View view) {
+        Intent resultIntent = new Intent(this, PuzzleResult.class);
+        resultIntent.putExtra("pic", "picText");
+        startActivity(resultIntent);
     }
 }

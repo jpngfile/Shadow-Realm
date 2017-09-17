8a7335feb1309d7bd2b6149be6720c445da26087
Bohan Chen <danielchen97dc@gmail.com>
2017-09-16 23:48:09 -0400

    enabled stage preview and camera operations, removed camera screen from main menu

--- app/src/main/java/com/example/jason/shadowgame/PuzzleStage.java
+++ app/src/main/java/com/example/jason/shadowgame/PuzzleStage.java
@@ -1,13 +1,10 @@
 package com.example.jason.shadowgame;
-    import static android.widget.ImageView.ScaleType.CENTER;
-    import static android.widget.ImageView.ScaleType.CENTER_CROP;
-    import static android.widget.ImageView.ScaleType.CENTER_INSIDE;
-    import static android.widget.ImageView.ScaleType.FIT_CENTER;
-    import static android.widget.ImageView.ScaleType.FIT_START;
-    import static android.widget.ImageView.ScaleType.FIT_XY;
-    import static com.example.jason.shadowgame.R.attr.background;
+
+import static android.widget.ImageView.ScaleType.FIT_XY;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,49 +12,37 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-    import android.Manifest;
-    import android.content.Intent;
-    import android.content.pm.PackageManager;
-    import android.net.Uri;
-    import android.os.Environment;
-    import android.provider.MediaStore;
-    import android.support.v4.app.ActivityCompat;
-    import android.support.v4.content.ContextCompat;
-    import android.support.v7.app.AppCompatActivity;
-    import android.os.Bundle;
-    import android.view.View;
-    import android.view.ViewManager;
-    import android.widget.Button;
-    import android.widget.ImageView;
-    import android.widget.TextView;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 
-    import java.io.File;
-    import java.text.SimpleDateFormat;
-    import java.util.Date;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class PuzzleStage extends AppCompatActivity {
-    private Button takePictureButton;
-    private ImageView imageView;
-    private ImageView border;
-    private TextView prompt;
+    int level = -1;
+    Button stageStart, stageBack;
+    TextView stageLevelLabel;
+    ImageView stageImage;
     Uri file;
-    static int x = 0;
+
+    final int CORNER_WIDTH = 30;
+
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         if (requestCode == 0) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED
                     && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
-                takePictureButton.setEnabled(true);
+                stageStart.setEnabled(true);
             }
         }
     }
-
-
-    int level = -1;
-    Button stageStart, stageBack;
-    TextView stageLabel;
-    ImageView stageImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,28 +55,24 @@ public class PuzzleStage extends AppCompatActivity {
         }
         setContentView(R.layout.activity_puzzle_stage);
 
-        takePictureButton = (Button) findViewById(R.id.button_image);
-        imageView = (ImageView) findViewById(R.id.imageview);
-        border = (ImageView) findViewById(R.id.border);
-        prompt = (TextView) findViewById(R.id.prompt);
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            takePictureButton.setEnabled(false);
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
-        }
-    }
-
-        stageLabel = (TextView) findViewById(R.id.stageLevelLabel);
-        stageLabel.setText("Level " + level);
+        stageLevelLabel = (TextView) findViewById(R.id.stageLevelLabel);
+        stageLevelLabel.setText("Level " + level);
 
         stageImage = (ImageView) findViewById(R.id.stageImage);
-        stageImage.setImageResource(getDrawableId (level));
+        displayImage(stageImage, BitmapFactory.decodeResource(getResources(), getDrawableId (level)));
 
         stageStart = (Button) findViewById(R.id.stageStart);
         stageStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                File outFile = getOutputMediaFile();
+                takePicture();
+                Log.d("PuzzleCamera", outFile.getAbsolutePath());
+                stageLevelLabel.setText("Level " + level + "[" + file.getPath() + "]");
+                /*
                 Intent i = new Intent(PuzzleStage.this, PuzzleCamera.class);
-                startActivity (i);
+                i.putExtra("fileUrl", outFile.getAbsolutePath());
+                startActivity (i);*/
             }
         });
 
@@ -102,41 +83,29 @@ public class PuzzleStage extends AppCompatActivity {
                 finish ();
             }
         });
+
+        Util.checkPermission(this, Manifest.permission.CAMERA);
+        Util.checkPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
     }
 
-    private int getDrawableId (int level) {
-        return getResources().getIdentifier("level_" + level, "drawable", "com.example.jason.shadowgame");
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 100) {
+            if (resultCode == RESULT_OK) {
+                trimPicture(file);
+                displayImage(stageImage, BitmapFactory.decodeFile(file.getPath()));
+                stageImage.setScaleType(FIT_XY);
+            }
+        }
     }
-}
 
-
-    public void takePicture(View view) {
+    private void takePicture() {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         file = Uri.fromFile(getOutputMediaFile());
         intent.putExtra(MediaStore.EXTRA_OUTPUT, file);
 
         startActivityForResult(intent, 100);
     }
-
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == 100) {
-            if (resultCode == RESULT_OK) {
-
-
-                imageView.setImageURI(file);
-
-                imageView.setRotation(90);
-
-                imageView.setScaleType(FIT_XY);
-                //imageView.setScaleType(FIT_CENTER);
-                border.setVisibility(View.INVISIBLE);
-                prompt.setVisibility(View.INVISIBLE);
-            }
-        }
-    }
-
 
     private static File getOutputMediaFile(){
         File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
@@ -147,10 +116,38 @@ public class PuzzleStage extends AppCompatActivity {
                 return null;
             }
         }
-        x++;
-//        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        return new File(mediaStorageDir.getPath() + File.separator +
-                "test"+x+".jpg");
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        return new File(mediaStorageDir.getPath() + File.separator + timeStamp + ".jpg");
+    }
+
+    private int getDrawableId (int level) {
+        return getResources().getIdentifier("level_" + level, "drawable", "com.example.jason.shadowgame");
+    }
+
+    private void displayImage (ImageView view, Bitmap bm) {
+        view.setImageBitmap(Util.getRoundedCornerBitmap(bm, CORNER_WIDTH));
+    }
+
+    private void trimPicture (Uri uri) {
+        Bitmap original = BitmapFactory.decodeFile(uri.getPath());
+        int margin = (original.getHeight() - original.getWidth()) / 2;
+        Bitmap modified = Bitmap.createBitmap(original, 0, margin, original.getWidth(), original.getWidth());
+        FileOutputStream out = null;
+        try {
+            out = new FileOutputStream(uri.getPath());
+            modified.compress(Bitmap.CompressFormat.PNG, 100, out); // bmp is your Bitmap instance
+            // PNG is a lossless format, the compression factor (100) is ignored
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (out != null) {
+                    out.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
 
